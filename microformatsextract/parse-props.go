707cbe10a1aab932b6fb4a2bdbceafc453e94a89
Tiim <tim.bachmann96@gmail.com>
2022-11-16 11:09:35 +0100

    feat: h-cite and h-entry extraction

--- microformatsextract/parse-props.go
+++ microformatsextract/parse-props.go
@@ -1,8 +1,38 @@
 package microformatsextract
 
 import (
+	"fmt"
+	"strings"
+	"time"
+
 	"willnorris.com/go/microformats"
 )
+
+type MF2HEntry struct {
+	Name      string
+	Summary   string
+	Content   string
+	Published *time.Time
+	Updated   *time.Time
+	Author    *MF2HCard
+	Category  []string
+	Url       string
+	InReplyTo *MF2HCite
+	RSVP      string
+	LikeOf    *MF2HCite
+	RepostOf  *MF2HCite
+}
+
+type MF2HCite struct {
+	Name        string
+	Published   *time.Time
+	Author      *MF2HCard
+	Url         string
+	Publication string
+	Accessed    *time.Time
+	Content     string
+	Summary     string
+}
 
 type MF2HCard struct {
 	Name string
@@ -13,16 +43,16 @@ type MF2HApp struct {
 	Name         string
 	Logo         string
 	Summary      string
-	Author       MF2HCard
+	Author       *MF2HCard
 	RedirectUris []string
 }
 
-func GetHApp(data *microformats.Data) MF2HApp {
+func GetHApp(data *microformats.Data) *MF2HApp {
 	item := getEntryWithType(data, "h-app", "h-x-app")
 	if item == nil {
-		return MF2HApp{}
+		return nil
 	}
-	return MF2HApp{
+	return &MF2HApp{
 		Url:          GetStringProp("url", item),
 		Name:         GetStringProp("name", item),
 		Logo:         GetStringProp("logo", item),
@@ -30,6 +60,68 @@ func GetHApp(data *microformats.Data) MF2HApp {
 		Author:       GetHCard("author", item),
 		RedirectUris: GetStringPropSlice("redirect-uri", item),
 	}
+}
+
+func GetHEntry(data *microformats.Data) *MF2HEntry {
+	item := getEntryWithType(data, "h-entry")
+	if item == nil {
+		return nil
+	}
+
+	return &MF2HEntry{
+		Name:      GetStringProp("name", item),
+		Summary:   GetStringProp("summary", item),
+		Content:   GetStringProp("content", item),
+		Published: GetTimeProp("published", item),
+		Updated:   GetTimeProp("updated", item),
+		Author:    GetHCard("author", item),
+		Category:  GetStringPropSlice("category", item),
+		Url:       GetStringProp("url", item),
+		InReplyTo: GetHCite("in-reply-to", item),
+		LikeOf:    GetHCite("like-of", item),
+		RepostOf:  GetHCite("repost-of", item),
+	}
+}
+
+func GetHCard(name string, item *microformats.Microformat) *MF2HCard {
+	author := item.Properties[name]
+	if len(author) > 0 {
+		if authorStr, ok := author[0].(string); ok {
+			return &MF2HCard{Name: authorStr}
+		}
+		authorMf, ok := author[0].(*microformats.Microformat)
+		if ok {
+			return &MF2HCard{Name: authorMf.Value}
+		}
+	}
+	return nil
+}
+
+func GetHCite(name string, item *microformats.Microformat) *MF2HCite {
+	cite, ok := item.Properties[name]
+	if !ok {
+		return nil
+	}
+	if len(cite) == 0 {
+		return nil
+	}
+	if citeString, ok := cite[0].(string); ok {
+		return &MF2HCite{Url: citeString}
+	}
+	citeMf, ok := cite[0].(*microformats.Microformat)
+	if ok {
+		return &MF2HCite{
+			Name:        GetStringProp("name", citeMf),
+			Published:   GetTimeProp("published", citeMf),
+			Author:      GetHCard("author", citeMf),
+			Url:         GetStringProp("url", citeMf),
+			Publication: GetStringProp("publication", citeMf),
+			Accessed:    GetTimeProp("accessed", citeMf),
+			Content:     GetStringProp("content", citeMf),
+			Summary:     GetStringProp("summary", citeMf),
+		}
+	}
+	return nil
 }
 
 func getEntryWithType(data *microformats.Data, types ...string) *microformats.Microformat {
@@ -53,8 +145,14 @@ func GetStringProp(name string, item *microformats.Microformat) string {
 	if len(propValue) == 0 {
 		return ""
 	}
-	value, _ := propValue[0].(string)
-	return value
+	if value, ok := propValue[0].(string); ok {
+		return trimLines(value)
+	}
+	if value, ok := propValue[0].(map[string]string); ok && value["value"] != "" {
+		return trimLines(value["value"])
+	}
+	fmt.Printf("Did not find string prop %s: %v (%T)", name, propValue[0], propValue[0])
+	return ""
 }
 
 func GetStringPropSlice(name string, item *microformats.Microformat) []string {
@@ -68,19 +166,89 @@ func GetStringPropSlice(name string, item *microformats.Microformat) []string {
 	slice := make([]string, 0, len(propValue))
 	for _, value := range propValue {
 		if value, ok := value.(string); ok {
-			slice = append(slice, value)
+			slice = append(slice, trimLines(value))
 		}
 	}
 	return slice
 }
 
-func GetHCard(name string, item *microformats.Microformat) MF2HCard {
-	author := item.Properties["name"]
-	if len(author) > 0 {
-		authorMf, ok := author[0].(*microformats.Microformat)
-		if ok {
-			return MF2HCard{Name: authorMf.Value}
+var timeFormats = []string{
+	time.RFC3339,
+	"2006-01-02T15:04:05-07:00",
+	"2006-01-02T15:04:05-0700",
+}
+
+func GetTimeProp(name string, item *microformats.Microformat) *time.Time {
+	propValue, ok := item.Properties[name]
+	if !ok {
+		return nil
+	}
+	if len(propValue) == 0 {
+		return nil
+	}
+	if value, ok := propValue[0].(string); ok {
+		var parsed time.Time
+		for _, format := range timeFormats {
+			var err error
+			parsed, err = time.Parse(format, value)
+			if err == nil {
+				break
+			}
+		}
+		if parsed.IsZero() {
+			return nil
+		}
+		parsed = parsed.UTC()
+		return &parsed
+	}
+	fmt.Printf("Did not find time prop %s: %v (%T)", name, propValue[0], propValue[0])
+	return nil
+}
+
+func (h *MF2HEntry) GetShortContent(maxLength, maxNewlines int) string {
+	if h.Content != "" && !isContentTooLong(h.Content, maxLength, maxNewlines) {
+		return h.Content
+	} else if h.Summary != "" && !isContentTooLong(h.Summary, maxLength, maxNewlines) {
+		return h.Summary
+	} else if h.Summary != "" {
+		return truncateContent(h.Summary, maxLength, maxNewlines)
+	} else if h.Content != "" {
+		return truncateContent(h.Content, maxLength, maxNewlines)
+	} else if h.Name != "" {
+		return truncateContent(h.Name, maxLength, maxNewlines)
+	}
+	return ""
+}
+
+func truncateContent(content string, maxLength, maxNewlines int) string {
+	truncated := false
+	if strings.Count(content, "\n") > maxNewlines {
+		content = strings.Join(strings.Split(content, "\n")[0:maxNewlines], "\n")
+		truncated = true
+	}
+	if len(content) > maxLength {
+		content = content[0:maxLength]
+		truncated = true
+	}
+	if truncated {
+		content = content + "..."
+	}
+	return content
+}
+
+func isContentTooLong(content string, maxLength, maxNewlines int) bool {
+	return len(content) > maxLength || strings.Count(content, "\n") > maxNewlines
+}
+
+func trimLines(s string) string {
+	s = strings.TrimSpace(s)
+	lines := strings.Split(s, "\n")
+	newLines := make([]string, 0)
+	for _, line := range lines {
+		trim := strings.TrimSpace(line)
+		if trim != "" {
+			newLines = append(newLines, trim)
 		}
 	}
-	return MF2HCard{}
+	return strings.Join(newLines, "\n")
 }
--- webmentions/check-enrich.go
+++ webmentions/check-enrich.go
@@ -1,7 +1,7 @@
 package webmentions
 
 import (
-	"strings"
+	"tiim/go-comment-api/microformatsextract"
 
 	"github.com/PuerkitoBio/goquery"
 	"willnorris.com/go/microformats"
@@ -15,120 +15,12 @@ func (c *microformatEnricherChecker) CheckMention(w *Webmention) error {
 
 func (c *microformatEnricherChecker) CheckDocument(gq *goquery.Document, w *Webmention) error {
 	data := microformats.ParseNode(gq.Nodes[0], w.SourceUrl())
-	for _, item := range data.Items {
-		if strInArray("h-entry", item.Type) {
-			extractAuthor(item, w)
-			extractContent(item, w)
-		}
-	}
+	hentry := microformatsextract.GetHEntry(data)
+
+	w.AuthorName = hentry.Author.Name
+	w.Content = hentry.GetShortContent(500, 4)
+
 	return nil
-}
-
-func strInArray(str string, arr []string) bool {
-	for _, v := range arr {
-		if v == str {
-			return true
-		}
-	}
-	return false
-}
-
-func extractAuthor(hentry *microformats.Microformat, w *Webmention) {
-	author := hentry.Properties["author"]
-	if len(author) > 0 {
-		authorMf, ok := author[0].(*microformats.Microformat)
-		if ok {
-			w.AuthorName = authorMf.Value
-		}
-	}
-}
-
-func extractContent(hentry *microformats.Microformat, w *Webmention) {
-	eContent := trimLines(getMF2EContent(hentry))
-	pSummary := trimLines(getMF2PSummary(hentry))
-	pName := trimLines(getMF2PName(hentry))
-	w.Content = truncateContentAndSummary(eContent, pSummary, pName)
-}
-
-func truncateContentAndSummary(content, summary, name string) string {
-	if content != "" && !isContentTooLong(content) {
-		return content
-	} else if summary != "" && !isContentTooLong(summary) {
-		return summary
-	} else if summary != "" {
-		return truncateContent(summary)
-	} else if content != "" {
-		return truncateContent(content)
-	} else if name != "" {
-		return truncateContent(name)
-	}
-	return ""
-}
-
-func isContentTooLong(content string) bool {
-	return len(content) > 500 || strings.Count(content, "\n") > 4
-}
-func truncateContent(content string) string {
-	truncated := false
-	if strings.Count(content, "\n") > 4 {
-		content = strings.Join(strings.Split(content, "\n")[0:4], "\n")
-		truncated = true
-	}
-	if len(content) > 500 {
-		content = content[0:500]
-		truncated = true
-	}
-	if truncated {
-		content = content + "..."
-	}
-	return content
-}
-
-func trimLines(s string) string {
-	s = strings.TrimSpace(s)
-	s = strings.ReplaceAll(s, "\n\n", "\n")
-	lines := strings.Split(s, "\n")
-	newLines := make([]string, len(lines))
-	for i, line := range lines {
-		trim := strings.TrimSpace(line)
-		if trim != "" {
-			newLines[i] = trim
-		}
-	}
-	return strings.Join(newLines, "\n")
-}
-
-func getMF2EContent(hentry *microformats.Microformat) string {
-	content := hentry.Properties["content"]
-	if len(content) > 0 {
-		contentMf, ok := content[0].(map[string]string)
-		if ok {
-			return contentMf["value"]
-		}
-	}
-	return ""
-}
-
-func getMF2PSummary(hentry *microformats.Microformat) string {
-	summary := hentry.Properties["summary"]
-	if len(summary) > 0 {
-		summary, ok := summary[0].(string)
-		if ok {
-			return summary
-		}
-	}
-	return ""
-}
-
-func getMF2PName(hentry *microformats.Microformat) string {
-	name := hentry.Properties["name"]
-	if len(name) > 0 {
-		name, ok := name[0].(string)
-		if ok {
-			return name
-		}
-	}
-	return ""
 }
 
 func NewMicroformatEnricherChecker() *microformatEnricherChecker {
