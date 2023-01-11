8d84f26feb1aff935a5b3412434c5a074489af66
Tiim <tim.bachmann96@gmail.com>
2023-01-11 10:34:58 +0100

    fix go error

--- api/api.go
+++ api/api.go
@@ -3,7 +3,6 @@ package api
 import (
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 	"tiim/go-comment-api/config"
 
--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-REMOTE=comments.tiim.ch
-export DOCKER_HOST="ssh://tim@$REMOTE"
-
-docker-compose up --build -d
